bfe508462f328527059711ccb97f43ab107f37cd
NikolayHD <hidalgo1984@list.ru>
2018-06-15 04:50:19 +0300

    приложение решает задачу для захардкоженных входных данных

---
+++ ExecutorsSelection/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExecutorsSelection
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double[] paymentRates = { 1, 1 };
+			double[] workQualities = { 0.5, 1 };
+			
+			// total words to translate
+			double workAmount = 100;
+			double[] availableWorktimes = { 1000, 1000 };
+			double[] workSpeeds = { 1, 1 };
+
+			// deltaRate is such an increase of payment rate
+			// that increasing rate by it when quality changes by deltaQuality
+			// keeps utility function (whose maximum we are seeking) unchanged
+			double deltaRate = 0.1;
+			double deltaQuality = 0.01;
+
+			double maxCost = 5000;
+			double minQuality = 0.5;
+
+			const int stagesCount = 1;
+
+			int[] executorStages = new int[] { 0, 0 };
+			int nExecutors = paymentRates.Length;
+
+			double[] b = new double[nExecutors + 2 + stagesCount * 2];
+
+			for (int i = 0; i < nExecutors; i++)
+				b[i] = availableWorktimes[i] * workSpeeds[i];
+
+			b[nExecutors] = maxCost;
+			b[nExecutors + 1] = -workAmount * minQuality;
+
+			for (int s = 0; s < stagesCount; s++)
+			{
+				int i = nExecutors + 2 + s * 2;
+				b[i] = workAmount;
+				b[i + 1] = -workAmount;
+			}
+
+			double[] c = new double[nExecutors];
+			for (int i = 0; i < nExecutors; i++)
+				c[i] = -paymentRates[i] + workQualities[i] * deltaRate / deltaQuality;
+
+			double[,] a = new double[b.Length, c.Length];
+
+			for (int i = 0; i < nExecutors; i++)
+				a[i, i] = 1;
+
+			for (int j = 0; j < nExecutors; j++)
+			{
+				a[nExecutors, j] = paymentRates[j];
+				a[nExecutors + 1, j] = -workQualities[j];
+
+				int index = nExecutors + 2 + executorStages[j] * 2;
+				a[index, j] = 1;
+				a[index + 1, j] = -1;
+			}
+
+			var simplex = new Simplex(b, c, a);
+			var result = simplex.Maximize();
+
+			Console.WriteLine($"Maximized value: {result.Value}");
+			Console.WriteLine("Vector:");
+
+			for (int i = 0; i < result.Vector.Length; i++)
+			{
+				double v = result.Vector[i];
+				Console.Write(v.ToString("F2"));
+				
+				if (i < result.Vector.Length - 1)
+					Console.Write(' ');
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ExecutorsSelection/Simplex.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExecutorsSelection
+{
+	public class Simplex
+	{
+		private readonly double[] _c;
+		private readonly double[,] _a;
+		private readonly double[] _b;
+		private readonly HashSet<int> _nonBasicSet = new HashSet<int>();
+		private readonly HashSet<int> _basicSet = new HashSet<int>();
+		private double _v;
+
+		public Simplex(double[] b, double[] c, double[,] a)
+		{
+			int bLen = b.Length;
+			int cLen = c.Length;
+
+			if (bLen != a.GetLength(0))
+				throw new ArgumentException("Number of constraints in A doesn't match number in b.");
+
+			if (cLen != a.GetLength(1))
+				throw new ArgumentException("Number of variables in c doesn't match number in A.");
+
+			// Extend max fn coefficients vector with 0 padding
+			_c = new double[cLen + bLen];
+			Array.Copy(c, _c, cLen);
+
+			// Extend coefficient matrix with 0 padding
+			_a = new double[cLen + bLen, cLen + bLen];
+
+			for (int i = 0; i < bLen; i++)
+				for (int j = 0; j < cLen; j++)
+					_a[i + cLen, j] = a[i, j];
+
+			// Extend constraint right-hand side vector with 0 padding
+			_b = new double[cLen + bLen];
+			Array.Copy(b, 0, _b, cLen, bLen);
+
+			// Populate non-basic and basic sets
+			for (int j = 0; j < cLen; j++)
+				_nonBasicSet.Add(j);
+
+			for (int i = 0; i < bLen; i++)
+				_basicSet.Add(cLen + i);
+		}
+
+		public (double Value, double[] Vector) Maximize()
+		{
+			while (true)
+			{
+				// Find highest coefficient for entering var
+				int enteringIndex = int.MinValue;
+				double cEntering = 0;
+
+				foreach (var j in _nonBasicSet)
+				{
+					if (_c[j] > cEntering)
+					{
+						cEntering = _c[j];
+						enteringIndex = j;
+					}
+				}
+
+				// If no coefficient > 0, there's no more maximizing to do, and we're almost done
+				if (enteringIndex == int.MinValue)
+					break;
+
+				// Find lowest check ratio
+				double minRatio = double.PositiveInfinity;
+				int leavingIndex = int.MinValue;
+
+				foreach (var i in _basicSet)
+				{
+					if (_a[i, enteringIndex] > 0)
+					{
+						double r = _b[i] / _a[i, enteringIndex];
+						if (r < minRatio)
+						{
+							minRatio = r;
+							leavingIndex = i;
+						}
+					}
+				}
+
+				// Unbounded
+				if (double.IsInfinity(minRatio))
+					return (double.PositiveInfinity, null);
+
+				pivot(enteringIndex, leavingIndex);
+			}
+
+			// Extract amounts and slack for optimal solution
+			int n = _b.Length;
+			var x = new double[n];
+
+			foreach (int i in _basicSet)
+				x[i] = _b[i];
+
+			/*for (var i = 0; i < n; i++)
+				if (_basicSet.Contains(i))
+					x[i] = _b[i];
+				else
+					x[i] = 0;*/
+
+			// Return max and variables
+			return (_v, x);
+		}
+
+		private void pivot(int enteringIndex, int leavingIndex)
+		{
+			_nonBasicSet.Remove(enteringIndex);
+			_basicSet.Remove(leavingIndex);
+
+			_b[enteringIndex] = _b[leavingIndex] / _a[leavingIndex, enteringIndex];
+
+			foreach (int j in _nonBasicSet)
+				_a[enteringIndex, j] = _a[leavingIndex, j] / _a[leavingIndex, enteringIndex];
+
+			_a[enteringIndex, leavingIndex] = 1 / _a[leavingIndex, enteringIndex];
+
+			foreach (int i in _basicSet)
+			{
+				_b[i] = _b[i] - _a[i, enteringIndex] * _b[enteringIndex];
+
+				foreach (int j in _nonBasicSet)
+					_a[i, j] = _a[i, j] - _a[i, enteringIndex] * _a[enteringIndex, j];
+
+				_a[i, leavingIndex] = -1 * _a[i, enteringIndex] * _a[enteringIndex, leavingIndex];
+			}
+
+			_v = _v + _c[enteringIndex] * _b[enteringIndex];
+
+			foreach (int j in _nonBasicSet)
+				_c[j] = _c[j] - _c[enteringIndex] * _a[enteringIndex, j];
+
+			_c[leavingIndex] = -1 * _c[enteringIndex] * _a[enteringIndex, leavingIndex];
+
+			_nonBasicSet.Add(leavingIndex);
+			_basicSet.Add(enteringIndex);
+		}
+	}
+}
